322892365d5ffb01c7a9e06156a1495266c0a4b1
tanjiro <madhanking2004ac@gmail.com>
2025-02-04 11:33:50 +0530

    Initial commit

--- neon.java
+++ neon.java
@@ -6,7 +6,9 @@ public class neon {
         System.out.println("Enter a number: ");
         int num = in.nextInt();
         int square = num * num;
-        int sum = 0;
+        int sum = 0;        
+        git branch -M main
+        git push -u origin main
 //9 -> 9*9 =  81;
 //81 -> 8+1 = 9;
         while (square > 0) {
